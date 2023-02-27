3dd47a3667957654459c13f4258669098c8f300a
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-02-27 11:13:13 -0600

    Encabezados

--- ProyectoInterprete/src/Main.java
+++ ProyectoInterprete/src/Main.java
@@ -1,3 +1,15 @@
+/*
+Profesor: Douglas Leonel Barrios
+Auxiliares: Fernanda Esquivel y Francisco Castillo
+INTEGRANTES: 
+Francis Gabriela Aguilar Leal, 22243
+Gustavo Adolfo Cruz Bardales, 22779
+Nancy Gabriela Mazariegos Molina, 22513
+27/01/2023
+Hoja de Trabajo 2
+Curso: Algoritmos y Estructuras de datos
+Sección: 20
+*/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
