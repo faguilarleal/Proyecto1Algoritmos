15682ecf9761f733548cbe4fb14c420b8e8a75e8
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-03-14 17:39:19 -0600

    Update ValidarExpresiones.java

--- ProyectoInterprete/src/Controlador/ValidarExpresiones.java
+++ ProyectoInterprete/src/Controlador/ValidarExpresiones.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+import java.util.regex.*;
+import java.util.*;
 package Controlador;
 
 public class ValidarExpresiones {
+    Scanner valor = new Scanner(System.in);
+    Pattern validacion = Pattern.compile("()");
+    String z = valor;
+    Matcher x =validacion.matcher(z);
+    
 }
