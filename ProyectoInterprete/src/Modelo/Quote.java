eabdc8e9e89b4083f8cb95d8ba10e55f68e1c982
G2309 <106942151+G2309@users.noreply.github.com>
2023-03-14 18:27:10 -0600

    Update Quote.java

--- ProyectoInterprete/src/Modelo/Quote.java
+++ ProyectoInterprete/src/Modelo/Quote.java
@@ -1,9 +1,12 @@
 package Modelo;
+import java.util.*;
 
 public class Quote {
+	//Constructor
     public Quote(){
 
     }
+    //Metodo para convertir el texto ingresado a todo mayusculas
     public String mayus(String cadena){
         String cad="";
         for (int n = 0;n <cadena.length (); n ++) { 
@@ -11,5 +14,15 @@ public class Quote {
             cad+=Character.toUpperCase(c);
         }
         return cad;
+    }
+    //Metodo que devuelve como String cada elemento luego de la palabra QUOTE o '
+    public String QUOTE(ArrayList<String> expresion){
+    	String quote = "";
+    	for(int i =1; i<expresion.size();i++) 
+    	{
+    		//Se empieza a concatenar los elementos de expresion en quote, luego de la primera posicion que es donde se encuentra la palabra reservada QUOTE
+    		quote += expresion.get(i) + "";
+    	}
+    	return quote.trim(); //Retorna quote borrando espacios en blanco del principio o fin de la expresion
     }
 }
