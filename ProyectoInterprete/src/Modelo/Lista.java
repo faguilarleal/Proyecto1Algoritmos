6fab79d956e0a2f785a5fcf1d25c94532d99ad7b
G2309 <106942151+G2309@users.noreply.github.com>
2023-03-16 22:49:56 -0600

    Add files via upload

---
+++ ProyectoInterprete/src/Modelo/Lista.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Lista {
+	private static String st = new String();
+	public Lista() {
+		
+	}
+	
+	public List<String> LIST(ArrayList<String> expresion) {
+		List<String> lista = new ArrayList<>();
+		for(int i = 1; i<expresion.size();i++ ) 
+		{
+			while (!expresion.get(i).equals(")")) {
+				lista.add(expresion.get(i));
+			}
+		}
+		return lista;
+	}
+	
+}
