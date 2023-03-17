b4c90687a50b03ea8dfcc4e0230a41de7a571f3b
G2309 <106942151+G2309@users.noreply.github.com>
2023-03-17 17:38:51 -0600

    Lector de archivos

--- ProyectoInterprete/src/Modelo/LectorArchivo.java
+++ ProyectoInterprete/src/Modelo/LectorArchivo.java
@@ -6,18 +6,13 @@ public class LectorArchivo {
 	
 	public ArrayList<String> LeerArchivo() throws IOException{
 		ArrayList<String> expresion = new ArrayList<>();
+		String linea = "";
 		try {
-			BufferedReader lector = new BufferedReader(new FileReader("/home/gustavo/eclipse-workspace/Proyecto 1/Proyecto1Algoritmos-main/ProyectoInterprete/src/Modelo/archivo.txt"));
-			String linea =  lector.readLine();
-			while(linea != null) {
-				StringTokenizer tokens = new StringTokenizer(linea, " \t\r\n"); 
-				while(tokens.hasMoreTokens()) {
-					String st = tokens.nextToken();
-					expresion.add(st);
-				}
-				linea = lector.readLine();
+			Scanner lector = new Scanner(new FileReader("./src/Texto.txt"));
+			while(lector.hasNextLine()) {
+				linea += lector.nextLine();
 			}
-			
+			expresion.add(linea);
 		}catch(IOException e){
 			System.out.println("No es posible leer el archivo");
 		}
