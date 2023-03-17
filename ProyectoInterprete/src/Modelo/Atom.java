93f9e20d8933795f830b9012fc7cc035e0ec7fa3
G2309 <106942151+G2309@users.noreply.github.com>
2023-03-16 20:04:06 -0600

    Add files via upload

---
+++ ProyectoInterprete/src/Modelo/Atom.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Atom {
+	private static boolean b;
+	public Atom() {
+		
+	}
+	
+	public boolean ATOM(ArrayList<String> expresion) {
+		int ultimo = expresion.size()-1;
+		for(int i = 1; i<expresion.size();i++) 
+		{
+			if(expresion.size()== 0) {
+				b = false;
+			}
+			else if(expresion.get(i).equals("(") && expresion.get(ultimo).equals(")")) 
+			{
+				b = false;
+			}else {
+				b = true;
+			}
+		}
+		return b;
+	}
+	
+}
