4ef94f1c1fae853c444191750690243089e6139d
G2309 <106942151+G2309@users.noreply.github.com>
2023-03-17 19:35:23 -0600

    Update Quote.java

--- ProyectoInterprete/src/Modelo/Quote.java
+++ ProyectoInterprete/src/Modelo/Quote.java
@@ -7,16 +7,6 @@ public class Quote {
 
     }
 
-    //Metodo que devuelve como String cada elemento luego de la palabra QUOTE o '
-    public String QUOTE(ArrayList<Object> expresion){
-    	String quote = "";
-    	for(int i =1; i<expresion.size();i++) 
-    	{
-    		//Se empieza a concatenar los elementos de expresion en quote, luego de la primera posicion que es donde se encuentra la palabra reservada QUOTE
-    		quote += expresion.get(i) + "";
-    	}
-    	return quote; //Retorna quote borrando espacios en blanco del principio o fin de la expresion
-    }
 
     /**
      * Convierte una lista de objetos en un string
